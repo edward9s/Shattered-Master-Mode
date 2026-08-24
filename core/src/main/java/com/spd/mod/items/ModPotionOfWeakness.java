d3b0944b25289595690a53b0430b136145f383a2
edward9s <277983297+edward9s@users.noreply.github.com>
2026-08-25 06:29:00 +0800

    Centralize Weakness potion invariants in reset

--- core/src/main/java/com/spd/mod/items/ModPotionOfWeakness.java
+++ core/src/main/java/com/spd/mod/items/ModPotionOfWeakness.java
@@ -11,9 +11,6 @@ public class ModPotionOfWeakness extends Potion {
     public ModPotionOfWeakness() {
         super();
         this.level(-1);
-        this.icon = ItemSpriteSheet.Icons.SCROLL_TERROR;
-        this.keptThoughLostInvent = true;
-        this.unique = true;
     }
     
     @Override
@@ -24,6 +21,7 @@ public class ModPotionOfWeakness extends Potion {
     @Override
     public void reset() {
         this.image = ItemSpriteSheet.POTION_HOLDER;
+        this.icon = ItemSpriteSheet.Icons.SCROLL_TERROR;
         this.color = "weakness_potion";
         this.keptThoughLostInvent = true;
         this.unique = true;
@@ -34,8 +32,7 @@ public class ModPotionOfWeakness extends Potion {
         // 在讀取存檔前將等級歸零，抵消建構子的預設值，避免 Item 原生機制的 upgrade 疊加
         this.level(0);
         super.restoreFromBundle(bundle);
-        this.keptThoughLostInvent = true;
-        this.unique = true;
+        reset();
     }
 
     @Override
