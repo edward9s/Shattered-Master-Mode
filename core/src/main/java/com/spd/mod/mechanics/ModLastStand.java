475fd03fc579c164742ca432bddd6c7d3eae3351
edward9s <277983297+edward9s@users.noreply.github.com>
2026-09-08 16:30:54 +0800

    Restore Last Stand pale blue tint

--- core/src/main/java/com/spd/mod/mechanics/ModLastStand.java
+++ core/src/main/java/com/spd/mod/mechanics/ModLastStand.java
@@ -180,7 +180,7 @@ public class ModLastStand extends Buff {
 
     @Override
     public void tintIcon(Image icon) {
-        icon.hardlight(0xFFD15C);
+        icon.hardlight(0xDDEEFF);
     }
 
     @Override
