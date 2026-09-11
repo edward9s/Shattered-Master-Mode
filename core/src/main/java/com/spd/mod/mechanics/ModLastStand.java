a4fe86098764a8b95225ae365b85259245e63cd2
edward9s <277983297+edward9s@users.noreply.github.com>
2026-09-11 22:49:11 +0800

    Change tint color in tintIcon method

--- core/src/main/java/com/spd/mod/mechanics/ModLastStand.java
+++ core/src/main/java/com/spd/mod/mechanics/ModLastStand.java
@@ -242,7 +242,7 @@ public class ModLastStand extends Buff {
 
     @Override
     public void tintIcon(Image icon) {
-        icon.hardlight(0xFFD54A);
+        icon.hardlight(0xFFF176);
     }
 
     @Override
